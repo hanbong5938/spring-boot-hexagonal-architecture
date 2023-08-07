a32d41f7e3e9d4e85c534183996135fb5726e71a
han <hanbong5938@gmail.com>
2023-08-07 16:13:39 +0900

    [FEATURE] spring-boot-hexagonal-architecture-1 init 추가

--- domain/src/main/kotlin/com.example.domain/entity/Product.kt
+++ domain/src/main/kotlin/com.example.domain/entity/Product.kt
@@ -8,4 +8,9 @@ data class Product(
     val name: String,
     val money: Money,
     val status: ProductStatus
-)
+) {
+    init {
+        require(id > 0) { "Product id must be greater than 0" }
+        require(name.isNotBlank()) { "Product name must not be blank" }
+    }
+}
